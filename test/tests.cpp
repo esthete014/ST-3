5f4e8df6e89e01c7b52bfd8a3eb580ccfd7e63fa
esthete014 <nikkochetov@outlook.com>
2024-04-07 21:23:45 +0300

    Kochetov Nikolay st-3

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,6 +1,37 @@
-// Copyright 2021 GHA Test Team
+// Copyright 2024 Kochetov Nikolay
 
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
-#include <cstdint>
 #include "TimedDoor.h"
+
+class MockTimerClient : public TimerClient {
+public:
+    MOCK_METHOD(void, Timeout, (), (override));
+};
+
+TEST(TimedDoorTest, TestDoorOpen) {
+    int timeout = 5;
+    MockTimerClient mockTimer;
+    TimedDoor timedDoor(timeout);
+    timedDoor.unlock();
+
+    EXPECT_CALL(mockTimer, Timeout()).Times(1);
+
+    timedDoor.throwState();
+}
+
+TEST(TimedDoorTest, TestDoorClose) {
+    int timeout = 5;
+    MockTimerClient mockTimer;
+    TimedDoor timedDoor(timeout);
+    timedDoor.lock();
+
+    EXPECT_CALL(mockTimer, Timeout()).Times(0);
+
+    timedDoor.throwState();
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
